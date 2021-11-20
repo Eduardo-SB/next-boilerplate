f74f4763d3c56348f4929e9e625fc34755e33e25
Eduardo SB <eduardosb95@gmail.com>
2021-11-20 16:25:04 -0300

    Feat - Settings prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
-export default function Home({ title = 'Next' } : Props) {
+export default function Home({ title = 'Next' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -69,5 +69,5 @@ export default function Home({ title = 'Next' } : Props) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
